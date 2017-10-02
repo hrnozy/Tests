6578d1f88087f3b6e40c49300f2c1fd5be8db5b0
harun ozay <harun.ozay@n11.com>
2017-10-02 17:40:32 +0300

    noMessage

--- AllTests/src/test/java/TestHarun.java
+++ AllTests/src/test/java/TestHarun.java
@@ -1,6 +1,21 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestHarun {
 
-    public void shouldLogin() {
+    //Scenerio 1
+    @Test
+    public void shouldLoginWithFacebook() {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/user/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.n11.com/");
+        driver.manage().window().maximize();
+        driver.findElement(By.className("btnSignIn")).click();
+
+
 
 
     }
